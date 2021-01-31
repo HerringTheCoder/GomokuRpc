2b9340e72237cd1b8e4e5699b1b678d8e5157ac3
HerringTheCoder <szymon.sandura@gmail.com>
2021-01-31 10:18:27 +0100

    Add project files.

---
+++ Models/GameBoard.cs
@@ -0,0 +1,11 @@
+﻿using GomokuRpc.Protos;
+
+namespace GomokuRpc.Models
+{
+    public class Game
+    {
+        public int[,] Board = new int[15, 15];
+        public PlayerId CurrentPlayer = PlayerId.Undefined;
+        public int TurnCounter { get; set; } = 0;
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using GomokuRpc.Services;
+
+namespace GomokuRpc
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGrpcService<GameService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("This service requires gRPC client supporting HTTP/2 protocol");
+                });
+            });
+        }
+    }
+}
